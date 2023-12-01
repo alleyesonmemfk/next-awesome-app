c0cb56a3e85d08599204a70d834816fd928743c4
alleyesonmemfk <vadimwest0@gmail.com>
2023-12-01 09:37:28 +0600

    add Google authentification with NextAuth.js

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+	],
+	// TODO GITHUB PROVIDER
+})
